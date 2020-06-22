06575a5818ee78b2682fcf26404b196f3a52bed6
rahulb <rahulb@sahajsoft.com>
2020-06-22 13:36:40 +0530

    Extract function for print messages

--- main.py
+++ main.py
@@ -1,29 +1,38 @@
 import sys
+from typing import List
 from src.log import parse, build_tree, what_went_wrong, in_order_traversal
-
-args = sys.argv
-lines_to_parse = int(args[1])
-log_file_path = args[2]
-
-parsed_messages = parse(file_path=log_file_path, no_of_lines=lines_to_parse)
-print("-----------Parsed Messages---------------")
-for msg in parsed_messages:
-    print(msg)
-print("-----------------------------------------\n")
-
-print("-----------Build MessageTree---------------")
-message_tree = build_tree(log_messages=parsed_messages)
-print("-----------------------------------------\n")
-
-print("-----------Sorted Messages---------------")
-sorted_msg = in_order_traversal(message_tree)
-for msg in sorted_msg:
-    print(msg)
-print("-----------------------------------------\n")
+from src.message import Message
 
 
-print("-----------What Went Wrong---------------")
-errors = what_went_wrong(parsed_messages)
-for msg in errors:
-    print(msg)
-print("-----------------------------------------\n")
+def print_messages(messages: List[Message]):
+    for msg in messages:
+        print(msg)
+
+
+def main():
+    args = sys.argv
+    lines_to_parse = int(args[1])
+    log_file_path = args[2]
+
+    parsed_messages = parse(file_path=log_file_path, no_of_lines=lines_to_parse)
+    print("-----------Parsed Messages---------------")
+    print_messages(parsed_messages)
+    print("-----------------------------------------\n")
+
+    print("-----------Build MessageTree---------------")
+    message_tree = build_tree(log_messages=parsed_messages)
+    print("-----------------------------------------\n")
+
+    print("-----------Sorted Messages---------------")
+    sorted_msg = in_order_traversal(message_tree)
+    print_messages(sorted_msg)
+    print("-----------------------------------------\n")
+
+    print("-----------What Went Wrong---------------")
+    errors = what_went_wrong(parsed_messages)
+    print_messages(errors)
+    print("-----------------------------------------\n")
+
+
+if __name__ == '__main__':
+    main()
